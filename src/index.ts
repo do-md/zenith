ab5b0ae994bbb0c10c1ca69f0fa66054b8cbe20b
jintao-wang <wangjintaoapp@gmail.com>
2025-12-14 21:28:26 +0800

    chore: bump version to 1.1.0 - export memo decorator

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { BaseStore as ZenithStore } from './core/BaseStore';
 export { addEffect } from './core/effect';
+export { memo } from './core/memo';
 export { useContextStore, useContextGetter } from './react/useContextStore';
 export { useStore, useGetter } from './react/useStore';
 export type { ExtractState } from './type';
